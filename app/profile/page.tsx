294ebd114398b51d48e38bbc76dcd3ad2164563a
venumadhav17 <kalyanamvenumadhav@gmail.com>
2025-04-23 19:51:25 +0530

    used original approach in next.js

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,23 +1,17 @@
-"use client";
-
 import axios from "axios";
-import { useEffect, useState } from "react";
 
-export default function Profile() {
-  const [profilePicture, setProfilePicture] = useState("");
+export default async function Profile() {
+  /*const res = await axios.get("http:/localhost:3000/api/profile", {
+    headers: {
+      authorization: localStorage.getItem("token")
+    }
+  });
+  const profilePicture = res.data.avatarUrl;*/
 
-  // JS ran on my browser -> Not getting benefits of CSR
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/api/profile", {
-        headers: {
-          authorization: localStorage.getItem("token")
-        }
-      })
-      .then((res) => {
-        setProfilePicture(res.data.avatarUrl);
-      });
-  }, []);
+  // const profilePicture = 'http://cat.png';
+  // No-Follow-Up request that needs to go from the client in the network tab
+
+  // It's not going out from JS here. It's going out from the browser. /profile I can't read ?localStorage from the route itself
 
   return <div>{profilePicture}</div>;
 }
